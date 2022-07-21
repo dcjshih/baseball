da1615327887c8eb1ea523b0b280d270a1bd8655
Debbie Shih <dcjshih@umich.edu>
2022-07-21 14:54:01 -0400

    Added login and logout links and installed devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'public#landing', as: 'public_landing'
   get "public/directory", to: redirect('/cards')
   get "public/directory", as: :directory
